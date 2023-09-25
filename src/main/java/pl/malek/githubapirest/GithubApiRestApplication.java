a0694e19701ab47f7c603ccb201ddc0fd24088e6
Michal Malek <michal.malek98@wp.pl>
2023-09-25 10:35:43 +0200

    init

---
+++ src/main/java/pl/malek/githubapirest/GithubApiRestApplication.java
@@ -0,0 +1,13 @@
+package pl.malek.githubapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubApiRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/malek/githubapirest/GithubApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.malek.githubapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
